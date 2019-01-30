6ef76e100d749485cd0b49f6001f7617964c097c
Steven Haufe <haufes@hotmail.com>
2019-01-29 21:33:03 -0500

    Change to from MCP3008 to and MCP3304

--- pool_pressure_sensor.py
+++ pool_pressure_sensor.py
@@ -1,14 +1,13 @@
 import time
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_MCP3008
-SPI_PORT   = 0
-SPI_DEVICE = 0
-mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+from gpiozero import MCP3304
+
+MCP3304(channel=0, device=0)
+mcp = MCP3304(channel=0, device=0)
+
 values = []
-#take 30 measurements and average to even out the jitter
-for x in range(0, 30):
-   values.append(float(mcp.read_adc(0)))
-   time.sleep(.3)
+for x in range(0, 10):
+   values.append(float(mcp.value))
+   time.sleep(.1)
 
 #print values
 #values.sort()
@@ -18,4 +17,4 @@ value = sum(values)/len(values)
 #print value
 
 
-print '{{ "raw":"{0}", "psi":"{1}" }}'.format(value, round(float(value) * 5/22  - 260/11, 2))
+print '{{ "raw":"{0}", "psi":"{1}"}}'.format(value, round(float(value) * 85.322921661613  - 8.6354168622984, 2))
