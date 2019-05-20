186864017e754d6165a8796568ac5380f341c44a
Steven Haufe <haufes@hotmail.com>
2019-05-20 15:02:12 -0400

    Update presure sensor

--- pool_pressure_sensor.py
+++ pool_pressure_sensor.py
@@ -7,7 +7,7 @@ mcp = MCP3304(channel=0, device=0)
 values = []
 for x in range(0, 10):
    values.append(float(mcp.value))
-   time.sleep(.1)
+   time.sleep(.2)
 
 #print values
 #values.sort()
@@ -17,4 +17,11 @@ value = sum(values)/len(values)
 #print value
 
 
-print '{{ "raw":"{0}", "psi":"{1}"}}'.format(value, round(float(value) * 85.322921661613  - 8.6354168622984, 2))
+#print '{{ "raw":"{0}", "psi":"{1}"}}'.format(value, round(float(value) * 85.322921661613  - 8.6354168622984, 2))
+print '{{ "raw":"{0}", "psi":"{1}"}}'.format(value, round(float(value) * 231.86680571671 - 23.506555422694, 2))
+
+#https://keisan.casio.com/exec/system/1223508685
+
+#MCP3304
+#0 PSI = 0.101379562935 
+#4.75 PSI = 0.121865462093
